0dcb08fb93a5dc5c0bca2a3b08dae0c722f16ab0
Martin Lokvenc <lokvenc.martin@gmail.com>
2024-09-30 22:09:44 +0200

    User registration and token authentification endpoints created

--- users/serializers.py
+++ users/serializers.py
@@ -1,17 +1,25 @@
-from . import models
 from rest_framework.fields import CharField, EmailField
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    """Base serializer for User model"""
+    """Base serializer for default User model"""
 
-    username = CharField(required=True)
-    email = EmailField(required=True)
-
+    
     class Meta:
-        model = models.User
+        model = User
         fields = (
             "username",
             "email",
+            "password"
+        )
+        extra_kwargs = {'password': {'write_only': True}} # Ensures password won't be written as output from API
+
+    def create(self, validated_data):
+
+        return User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data.get("email"),
+            password=validated_data["password"]
         )
