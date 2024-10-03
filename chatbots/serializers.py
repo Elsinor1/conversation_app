48e00c53bf05b1e8c6f10e760c94362c1897d500
Martin Lokvenc <lokvenc.martin@gmail.com>
2024-10-03 23:10:40 +0200

    Chatbot messages endpoints created. LanguageLevel model updated.

--- chatbots/serializers.py
+++ chatbots/serializers.py
@@ -1,17 +1,36 @@
 from .models import Chat
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from rest_framework_json_api.serializers import PrimaryKeyRelatedField
 from conversations.models import Theme, Scenario
+from users.models import LanguageLevel
 
 
 
-class ChatModelSerializer(ModelSerializer):
+class ChatModelSerializer(serializers.ModelSerializer):
     theme = PrimaryKeyRelatedField(queryset=Theme.objects.all(), many=False)
     scenario = PrimaryKeyRelatedField(queryset=Scenario.objects.all(), many=False)
+    language_level = PrimaryKeyRelatedField(queryset=LanguageLevel.objects.all(), many=False)
 
     class Meta():
         model = Chat
         fields = (
             "theme",
-            "scenario"
+            "scenario",
+            "language_level"
         )
+
+
+class ChatMessagesSerializer(serializers.Serializer):
+    """
+    Serializer for validating chat_id
+    """
+    chat_id = serializers.CharField(max_length=16)
+    message = serializers.CharField(max_length=500, required=False)
+
+    def validate_chat_id(self, id):
+        """"
+        Validates that chat_id exists in the database
+        """
+        if not Chat.objects.get(pk=id):
+            raise serializers.ValidationError("Chat ID does not exists in the DB")
+        return id
--- chatbots/views.py
+++ chatbots/views.py
@@ -1,20 +1,22 @@
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import RetrieveModelMixin, ListModelMixin, UpdateModelMixin, CreateModelMixin, DestroyModelMixin
-from .models import Chat
-from conversations.models import Theme, Scenario
 from json import JSONDecodeError
-from .serializers import ChatModelSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.parsers import JSONParser
 from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.response import Response
 
+from .serializers import ChatModelSerializer, ChatMessagesSerializer
+from .models import Chat
+from conversations.models import Theme, Scenario
+from users.models import LanguageLevel
+from .chatbots import ConversationBot
 
 class ChatAPIVIewSet(
     GenericViewSet,
-    UpdateModelMixin,
     RetrieveModelMixin,
     ListModelMixin,
     DestroyModelMixin):
@@ -37,13 +39,13 @@ class ChatAPIVIewSet(
     def create(self, request):
         try:
             data = JSONParser().parse(request)
-            serializer = ChatModelSerializer(data=data)
+            serializer = ChatModelSerializer(data=data, raise_exception=True)
             if serializer.is_valid():
+                language_level = LanguageLevel.objects.get(pk=data["language_level"])
                 theme = Theme.objects.get(pk=data["theme"])
                 scenario = Scenario.objects.get(pk=data["scenario"])
-                # User is selected from the request
                 user = request.user
-                chat = Chat.objects.create(theme=theme, scenario=scenario, user=user)
+                chat = Chat.objects.create(theme=theme, scenario=scenario, user=user, language_level=language_level)
                 return Response(ChatModelSerializer(chat).data, status=status.HTTP_201_CREATED)
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -51,4 +53,34 @@ class ChatAPIVIewSet(
         except JSONDecodeError:
             return JsonResponse({"result": "error", "message": "Json decoding error"}, status=status.HTTP_400_BAD_REQUEST)
         
-    
+
+class ChatMessagesAPIView(APIView):
+    """
+    API View for communicating with chatbot
+    """
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+    def post(self, request):
+        try:
+            data = JSONParser().parse(request)
+            serializer = ChatMessagesSerializer(data, raise_exception=True)
+            if not serializer.is_valid():
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        except JSONDecodeError:
+            return JsonResponse({"result": "error", "message": "Json decoding error"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        chat = Chat.objects.get(pk=data["chat_id"])
+        chatbot = ConversationBot()
+
+        # If it is first message of the Chat
+        if not chat.is_started:
+            chatbot_message = chatbot.start_chat(chat=chat)
+        else:
+            chatbot_message = chatbot.continue_chat(chat=chat, human_message=data["message"])
+            chat.is_started = True
+            chat.save()
+        Response(chatbot_message, status=status.HTTP_200_OK)
